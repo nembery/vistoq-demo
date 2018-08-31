ffd79c09458eddfcd882d7e59c510626584157e8
Nathan <nembery@gmail.com>
2018-08-31 16:21:46 -0400

    update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
+echo $GCLOUD_SERVICE_KEY | base64 -d --ignore-garbage > ${HOME}/gcloud-service-key.json
 
 #
 # base.sh DIR TARGET BASE_NAME
