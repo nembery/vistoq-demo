7aa5668d64116f536b706f78ad8259f2f23b63ce
Nathan <nembery@gmail.com>
2018-08-31 16:10:34 -0400

    setup initial packer build

---
+++ scripts/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+echo $GCLOUD_SERVICE_KEY | base64 --decode --ignore-garbage > ${HOME}/gcloud-service-key.json
+
+#
+# base.sh DIR TARGET BASE_NAME
+DIR="$1"
+NAME="$2"
+BASE_NAME="$3"
+if [[ -z "$DIR" ]]; then
+    echo "please specify the directory as first runtime argument"
+    exit 1
+fi
+if [[ -z "$NAME" ]]; then
+    echo "please specify the name as second runtime argument"
+    exit 1
+fi
+
+SHA=$(git ls-tree HEAD "$DIR" | cut -d" " -f3 | cut -f1)
+#TAG_EXISTS=$(tag_exists $SHA)
+
+#if [ "$TAG_EXISTS" = "false" ]; then
+packer build ${DIR}/$NAME.json
+#else
+#    touch manifest-${NAME}.json
+#fi
