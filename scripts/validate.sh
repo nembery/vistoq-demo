aec78e81374e4184522ef87f11fbc988e441a7c2
Nathan <nembery@gmail.com>
2018-08-31 17:27:55 -0400

    add validation step

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -9,7 +9,7 @@ fi
 # base.sh DIR TARGET BASE_NAME
 DIR="$1"
 NAME="$2"
-BASE_NAME="$3"
+
 if [[ -z "$DIR" ]]; then
     echo "please specify the directory as first runtime argument"
     exit 1
@@ -19,11 +19,4 @@ if [[ -z "$NAME" ]]; then
     exit 1
 fi
 
-SHA=$(git ls-tree HEAD "$DIR" | cut -d" " -f3 | cut -f1)
-#TAG_EXISTS=$(tag_exists $SHA)
-
-#if [ "$TAG_EXISTS" = "false" ]; then
-packer build --force ${DIR}/$NAME.json
-#else
-#    touch manifest-${NAME}.json
-#fi
+packer validate ${DIR}/$NAME.json
